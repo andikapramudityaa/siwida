620a7ce3749e254eed4f9ed01ad8c4fbb912c1c4
rReyvn <andikapramudityaa@gmail.com>
2022-11-21 15:52:42 +0700

    Add Breadcrumb

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,43 +1,54 @@
 @extends('layouts.main')
 
 @section('body')
-    <div class="container mt-3 mb-3">
-        <x-success-alert />
+    <x-success-alert />
 
-        @if (session()->has('loginError'))
-            <div class="alert alert-danger alert-dismissible fade show mb-3" role="alert">
-                <i class="fa-solid fa-circle-xmark me-2"></i> {{ session('loginError') }}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        @endif
+    @if (session()->has('loginError'))
+        <div class="alert alert-danger alert-dismissible fade show mb-3" role="alert">
+            <i class="fa-solid fa-circle-xmark me-2"></i> {{ session('loginError') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
 
-        <h5 class="text-center fs-4 text-uppercase mt-5">Login Akun</h5>
-        <p class="text-center text-muted mb-3">Masukan identitas anda untuk melanjutkan</p>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="/" class="link-success text-decoration-none">
+                    <i class="fa-solid fa-house"></i>
+                </a>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">
+                Login
+            </li>
+        </ol>
+    </nav>
 
-        <div class="col d-flex justify-content-center">
-            <div class="col-lg-4">
-                <form action="/validate" method="POST">
-                    @csrf
-                    <div class="mb-3">
-                        <input type="text" name="username" class="form-control @error('username') is-invalid @enderror"
-                            id="username" maxlength="30" placeholder="Nama Pengguna" autofocus required
-                            value="{{ old('username') }}">
-                        <x-form-error-message id="username" />
-                    </div>
+    <h5 class="text-center fs-4 text-uppercase mt-5">Login Akun</h5>
+    <p class="text-center text-muted mb-3">Masukan identitas anda untuk melanjutkan</p>
 
-                    <div class="mb-4">
-                        <input type="password" name="password" class="form-control" id="password" maxlength="255"
-                            placeholder="Password" required>
-                    </div>
+    <div class="col d-flex justify-content-center">
+        <div class="col-lg-4">
+            <form action="/validate" method="POST">
+                @csrf
+                <div class="mb-3">
+                    <input type="text" name="username" class="form-control @error('username') is-invalid @enderror"
+                        id="username" maxlength="30" placeholder="Nama Pengguna" autofocus required
+                        value="{{ old('username') }}">
+                    <x-form-error-message id="username" />
+                </div>
 
-                    <button type="submit" class="btn btn-success w-100 mb-3">Masuk</button>
+                <div class="mb-4">
+                    <input type="password" name="password" class="form-control" id="password" maxlength="255"
+                        placeholder="Password" required>
+                </div>
 
-                    <div class="col d-flex justify-content-center">
-                        <p class="text-muted">Belum Punya Akun?&nbsp;</p>
-                        <a href="/users/create" class="text-decoration-none text-success">Daftar</a>
-                    </div>
-                </form>
-            </div>
+                <button type="submit" class="btn btn-success w-100 mb-3">Masuk</button>
+
+                <div class="col d-flex justify-content-center">
+                    <p class="text-muted">Belum Punya Akun?&nbsp;</p>
+                    <a href="/users/create" class="text-decoration-none text-success">Daftar</a>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,47 +1,58 @@
 @extends('layouts.main')
 
 @section('body')
-    <div class="container mt-3 mb-3">
-        <h5 class="text-center fs-4 text-uppercase mt-5">Daftar Akun</h5>
-        <p class="text-center text-muted mb-3">Masukan identitas anda untuk mendaftar</p>
-        <div class="col d-flex justify-content-center">
-            <div class="col-lg-4">
-                <form action="/users" method="POST">
-                    @csrf
-                    <div class="mb-3">
-                        <input type="text" name="username" class="form-control @error('username') is-invalid @enderror"
-                            id="username" maxlength="30" placeholder="Nama Pengguna" autofocus required
-                            value="{{ old('username') }}">
-                        <x-form-error-message id="username" />
-                    </div>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="/" class="link-success text-decoration-none">
+                    <i class="fa-solid fa-house"></i>
+                </a>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">
+                Register
+            </li>
+        </ol>
+    </nav>
 
-                    <div class="mb-3">
-                        <input type="text" name="name" class="form-control @error('name') is-invalid @enderror"
-                            id="name" maxlength="25" placeholder="Nama Lengkap" required value="{{ old('name') }}">
-                        <x-form-error-message id="name" />
-                    </div>
+    <h5 class="text-center fs-4 text-uppercase mt-5">Daftar Akun</h5>
+    <p class="text-center text-muted mb-3">Masukan identitas anda untuk mendaftar</p>
+    <div class="col d-flex justify-content-center">
+        <div class="col-lg-4">
+            <form action="/users" method="POST">
+                @csrf
+                <div class="mb-3">
+                    <input type="text" name="username" class="form-control @error('username') is-invalid @enderror"
+                        id="username" maxlength="30" placeholder="Nama Pengguna" autofocus required
+                        value="{{ old('username') }}">
+                    <x-form-error-message id="username" />
+                </div>
 
-                    <div class="mb-3">
-                        <input type="tel" pattern="^08[1-9][0-9]{7,10}$" minlength="10" maxlength="13"
-                            name="phoneNumber" class="form-control @error('phoneNumber') is-invalid @enderror"
-                            id="phoneNumber" placeholder="Nomor Telepon" value="{{ old('phoneNumber') }}" required>
-                        <x-form-error-message id="phoneNumber" />
-                    </div>
+                <div class="mb-3">
+                    <input type="text" name="name" class="form-control @error('name') is-invalid @enderror"
+                        id="name" maxlength="25" placeholder="Nama Lengkap" required value="{{ old('name') }}">
+                    <x-form-error-message id="name" />
+                </div>
 
-                    <div class="mb-3">
-                        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
-                            id="password" maxlength="255" placeholder="Password" required>
-                        <x-form-error-message id="password" />
-                    </div>
+                <div class="mb-3">
+                    <input type="tel" pattern="^08[1-9][0-9]{7,10}$" minlength="10" maxlength="13" name="phoneNumber"
+                        class="form-control @error('phoneNumber') is-invalid @enderror" id="phoneNumber"
+                        placeholder="Nomor Telepon" value="{{ old('phoneNumber') }}" required>
+                    <x-form-error-message id="phoneNumber" />
+                </div>
 
-                    <button type="submit" class="btn btn-success w-100 mb-3">Daftar</button>
+                <div class="mb-3">
+                    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
+                        id="password" maxlength="255" placeholder="Password" required>
+                    <x-form-error-message id="password" />
+                </div>
 
-                    <div class="col d-flex justify-content-center">
-                        <p class="text-muted">Sudah Punya Akun?&nbsp;</p>
-                        <a href="/login" class="text-decoration-none text-success">Masuk</a>
-                    </div>
-                </form>
-            </div>
+                <button type="submit" class="btn btn-success w-100 mb-3">Daftar</button>
+
+                <div class="col d-flex justify-content-center">
+                    <p class="text-muted">Sudah Punya Akun?&nbsp;</p>
+                    <a href="/login" class="text-decoration-none text-success">Masuk</a>
+                </div>
+            </form>
         </div>
     </div>
 @endsection
